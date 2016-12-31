ec9869317a54662ee2d68d61e47348cba32f34b1
mfscannell <markfscannell@gmail.com>
2016-12-31 11:01:46 -0600

    added _init__.py

--- unitTest/testVigenereCracker.py
+++ unitTest/testVigenereCracker.py
@@ -2,41 +2,37 @@
 import unittest
 from src.VigenereCracker import VigenereCracker
 
-
 class Test(unittest.TestCase):
-
 
     def setUp(self):
         pass
 
-
     def tearDown(self):
         pass
-
 
     def testInitialCandidateKeyLength(self):
         vigenereCracker = VigenereCracker(9, 4)
         initialCandidateKey = vigenereCracker.getInitialCandidateKey(9)
         self.assertTrue(len(initialCandidateKey) == 9)
         self.assertTrue(initialCandidateKey == 'AAAAAAAAA')
-        
+
     def testInitialCandidateKeyContent(self):
         vigenereCracker = VigenereCracker(9, 4)
         initialCandidateKey = vigenereCracker.getInitialCandidateKey(9)
         self.assertTrue(initialCandidateKey == 'AAAAAAAAA')
-        
+
     def testIncrementCandidateKey(self):
         vigenereCracker = VigenereCracker(3, 4)
         startKey = 'ABC'
         endKey = vigenereCracker.incrementCandidateKey(startKey)
         self.assertTrue(endKey == 'ABD')
-        
+
     def testIncrementCandidateKeyIncr(self):
         vigenereCracker = VigenereCracker(3, 4)
         startKey = 'ABZ'
         endKey = vigenereCracker.incrementCandidateKey(startKey)
         self.assertTrue(endKey == 'ACA')
-        
+
     def testIncrementCandidateKeyRoll(self):
         vigenereCracker = VigenereCracker(3, 4)
         startKey = 'ZZZ'
